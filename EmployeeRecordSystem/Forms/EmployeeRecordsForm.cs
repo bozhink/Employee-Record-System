615851e6ac7e7fe5465fa23ee8e21464cdd0131c
Bozhin Karaivanov <bozhink@gmail.com>
2016-03-04 17:33:41 +0200

    Added Settings.settings.

--- EmployeeRecordSystem/Forms/EmployeeRecordsForm.cs
+++ EmployeeRecordSystem/Forms/EmployeeRecordsForm.cs
@@ -17,12 +17,31 @@
 
         private void OpenToolStripMenuItem_Click(object sender, EventArgs e)
         {
+            if (!string.IsNullOrWhiteSpace(Settings.Default.InitialOpenDirectory))
+            {
+                string currentDirectory = this.openFileDialog.InitialDirectory;
+
+                try
+                {
+                    this.openFileDialog.InitialDirectory = Settings.Default.InitialOpenDirectory;
+                    currentDirectory = Settings.Default.InitialOpenDirectory;
+                }
+                catch
+                {
+                    this.openFileDialog.InitialDirectory = currentDirectory;
+                    Settings.Default.InitialOpenDirectory = currentDirectory;
+                }
+            }
+
             var result = this.openFileDialog.ShowDialog();
             if (result == DialogResult.OK)
             {
                 ////string fileName = this.openFileDialog.FileName;
                 ////MessageBox.Show(fileName);
             }
+
+            Settings.Default.InitialOpenDirectory = this.openFileDialog.InitialDirectory;
+            Settings.Default.Save();
         }
     }
 }
