6e9a491796ec84db35d29a398178d3f958ca6bf8
Bozhin Karaivanov <bozhink@gmail.com>
2016-03-04 19:16:07 +0200

    Added XmlSerializationService_Serialize_ShouldWork.

--- Services.Tests/EmployeeRecordSystem.Services.Tests/XmlSerializationServiceTests.cs
+++ Services.Tests/EmployeeRecordSystem.Services.Tests/XmlSerializationServiceTests.cs
@@ -130,5 +130,73 @@
             Assert.AreEqual(GradeType.A, firstCode.Details.Grade, "Grade should match.");
             Assert.AreEqual(1750.0m, firstCode.Details.Salary, "Salary should match.");
         }
+
+        [TestMethod]
+        public void XmlSerializationService_Serialize_ShouldWork()
+        {
+            var fileName = $"{ConfigurationManager.AppSettings["DataFilesPath"]}/employee-records.xml";
+
+            XmlReader reader = XmlReader.Create(fileName);
+            Assert.IsNotNull(reader, "Sample XML document is not loaded.");
+
+            var service = new XmlSerializationService();
+            Assert.IsNotNull(service, "Service should not be null");
+
+            var records = service.Deserialize<DataRecords>(reader).Result;
+            Assert.IsNotNull(records, "Deserialized records should not be null.");
+
+            string serializedRecords = service.Serialize(records).Result;
+            Assert.IsFalse(string.IsNullOrWhiteSpace(serializedRecords), "Serialized records should not be empty string.");
+
+            var newRecords = service.Deserialize<DataRecords>(serializedRecords, Encoding.UTF8).Result;
+            Assert.IsNotNull(newRecords, "Deserialized newRecords should not be null.");
+
+            var codes = newRecords.Codes;
+            Assert.IsNotNull(codes, "Deserialized codes should not be null.");
+
+            Assert.AreEqual(14, codes.Length, "Number of code items should be 14.");
+
+            var firstCode = codes[0];
+            Assert.AreEqual("E0001", firstCode.Id, "Id should match.");
+            Assert.AreEqual("Michael Perry", firstCode.EmployeeName, "EmployeeName should match.");
+            Assert.AreEqual(new DateTime(1999, 2, 2), firstCode.Details.DateOfJoin, "DateOfJoin should match.");
+            Assert.AreEqual(GradeType.A, firstCode.Details.Grade, "Grade should match.");
+            Assert.AreEqual(1750.0m, firstCode.Details.Salary, "Salary should match.");
+
+            for (int i = 0; i < records.Codes.Length; ++i)
+            {
+                Assert.IsTrue(this.CompareDataRecordsCode(records.Codes[i], newRecords.Codes[i]), $"Codes {i} should match.");
+            }
+        }
+
+        private bool CompareDataRecordsCode(DataRecordsCode code1, DataRecordsCode code2)
+        {
+            if (code1.Id != code2.Id)
+            {
+                return false;
+            }
+
+            if (code1.EmployeeName != code2.EmployeeName)
+            {
+                return false;
+            }
+
+            if (code1.Details.DateOfJoin != code2.Details.DateOfJoin)
+            {
+                return false;
+            }
+
+            if (code1.Details.Grade != code2.Details.Grade)
+            {
+                return false;
+            }
+
+            if (code1.Details.Salary != code2.Details.Salary)
+            {
+                return false;
+            }
+
+            return true;
+        }
     }
 }
