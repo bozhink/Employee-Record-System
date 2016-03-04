75b1929dde8bd79319e1dcea8056e4a900bd345d
Bozhin Karaivanov <bozhink@gmail.com>
2016-03-04 17:50:36 +0200

    Added PopulateTreeView.

--- EmployeeRecordSystem/Forms/EmployeeRecordsForm.cs
+++ EmployeeRecordSystem/Forms/EmployeeRecordsForm.cs
@@ -2,6 +2,7 @@
 {
     using System;
     using System.Windows.Forms;
+    using System.Xml;
 
     public partial class EmployeeRecordsForm : Form
     {
@@ -36,12 +37,60 @@
             var result = this.openFileDialog.ShowDialog();
             if (result == DialogResult.OK)
             {
+                this.PopulateTreeView(this.openFileDialog.FileName);
                 ////string fileName = this.openFileDialog.FileName;
                 ////MessageBox.Show(fileName);
             }
 
             Settings.Default.InitialOpenDirectory = this.openFileDialog.InitialDirectory;
             Settings.Default.Save();
+        }
+
+        private void PopulateTreeView(string fileName)
+        {
+            this.toolStripStatusLabel.Text = "Refreshing employee Codes. Please wait...";
+            this.Cursor = Cursors.WaitCursor;
+
+            this.treeView.Nodes.Clear();
+            var treeViewRootNode = new TreeNode("Employee Records");
+            this.treeView.Nodes.Add(treeViewRootNode);
+
+            TreeNodeCollection nodeCollection = treeViewRootNode.Nodes;
+            string stringValue = string.Empty;
+
+            var reader = XmlReader.Create(fileName);
+            reader.MoveToElement();
+            try
+            {
+                while (reader.Read())
+                {
+                    if (reader.HasAttributes && reader.NodeType == XmlNodeType.Element)
+                    {
+                        reader.MoveToElement();
+                        reader.MoveToElement();
+                        reader.MoveToAttribute("id");
+                        stringValue = reader.Value;
+                        reader.Read();
+                        reader.Read();
+
+                        if (reader.Name == "Dept")
+                        {
+                            reader.Read();
+                        }
+
+                        TreeNode codeNode = new TreeNode(stringValue);
+                        nodeCollection.Add(codeNode);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString());
+            }
+
+            this.Cursor = Cursors.Default;
+
+            this.toolStripStatusLabel.Text = "Click on an employee record to see their record.";
         }
     }
 }
