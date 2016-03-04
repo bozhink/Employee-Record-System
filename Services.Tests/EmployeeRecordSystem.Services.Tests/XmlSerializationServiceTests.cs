4a647b5594f353523520ae4f98680e837aafe875
Bozhin Karaivanov <bozhink@gmail.com>
2016-03-04 18:29:12 +0200

    Added XmlSerializationServiceTests.

---
+++ Services.Tests/EmployeeRecordSystem.Services.Tests/XmlSerializationServiceTests.cs
@@ -0,0 +1,15 @@
+﻿namespace EmployeeRecordSystem.Services.Tests
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class XmlSerializationServiceTests
+    {
+        [TestMethod]
+        public void XmlSerializationService_WithDefaultConstructor_ShouldReturnValidObject()
+        {
+            var service = new XmlSerializationService();
+            Assert.IsNotNull(service, "Service should not be null");
+        }
+    }
+}
