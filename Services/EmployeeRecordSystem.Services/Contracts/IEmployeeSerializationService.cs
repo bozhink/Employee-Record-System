c7660d5d93d26b73ef164e23289c508948efc35e
Bozhin Karaivanov <bozhink@gmail.com>
2016-03-04 16:58:09 +0200

    Added EmployeeSerializationService.

---
+++ Services/EmployeeRecordSystem.Services/Contracts/IEmployeeSerializationService.cs
@@ -0,0 +1,17 @@
+﻿namespace EmployeeRecordSystem.Services.Contracts
+{
+    using System.IO;
+    using System.Threading.Tasks;
+    using System.Xml;
+
+    using Models.Xml;
+
+    public interface IEmployeeSerializationService
+    {
+        Task<DataRecords> ReadEmployeeDataRecords(string xml);
+
+        Task<DataRecords> ReadEmployeeDataRecords(Stream xml);
+
+        Task<XmlDocument> SerializeEmployeeDataRecords(DataRecords records);
+    }
+}
---
+++ Services/EmployeeRecordSystem.Services/Services/EmployeeSerializationService.cs
@@ -0,0 +1,49 @@
+﻿namespace EmployeeRecordSystem.Services
+{
+    using System;
+    using System.IO;
+    using System.Threading.Tasks;
+    using System.Xml;
+
+    using Contracts;
+    using Models.Xml;
+
+    public class EmployeeSerializationService : IEmployeeSerializationService
+    {
+        private IXmlSerializationService service;
+
+        public EmployeeSerializationService(IXmlSerializationService service)
+        {
+            if (service == null)
+            {
+                throw new ArgumentNullException("service");
+            }
+
+            this.service = service;
+        }
+
+        public async Task<DataRecords> ReadEmployeeDataRecords(Stream xml)
+        {
+            return await this.service.Deserialize<DataRecords>(xml);
+        }
+
+        public async Task<DataRecords> ReadEmployeeDataRecords(string xml)
+        {
+            return await this.service.Deserialize<DataRecords>(xml);
+        }
+
+        public async Task<XmlDocument> SerializeEmployeeDataRecords(DataRecords records)
+        {
+            XmlDocument document = new XmlDocument
+            {
+                PreserveWhitespace = true
+            };
+
+            string xml = await this.service.Serialize(records);
+
+            document.LoadXml(xml);
+
+            return document;
+        }
+    }
+}
