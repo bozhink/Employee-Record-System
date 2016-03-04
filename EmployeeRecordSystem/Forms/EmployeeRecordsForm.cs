0e9001d5c01a50e9a9b8a51134d9592154e81d4a
Bozhin Karaivanov <bozhink@gmail.com>
2016-03-04 17:07:03 +0200

    Added openToolStripMenu.

--- EmployeeRecordSystem/Forms/EmployeeRecordsForm.cs
+++ EmployeeRecordSystem/Forms/EmployeeRecordsForm.cs
@@ -14,5 +14,15 @@
         {
             this.Close();
         }
+
+        private void OpenToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var result = this.openFileDialog.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                ////string fileName = this.openFileDialog.FileName;
+                ////MessageBox.Show(fileName);
+            }
+        }
     }
 }
